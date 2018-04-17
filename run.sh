9ed36009f269cdcc5c105748bf5030480f62ee01
Justyn Temme <justyntemme@gmail.com>
2018-04-17 09:50:09 -0500

    added whoami to make it non-user dependent

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 for line in $(cat domains.txt); do
 	touch reports-$line.txt
 	sudo chmod 666 reports-$line.txt
-	docker run -v /home/jtemme/Documents/vuln-reports/reports-$line.txt:/wpscan/output.txt -it --rm wpscanteam/wpscan --follow-redirection -t 2 -u "http://$line" --log /wpscan/output.txt
+	docker run -v /home/$(whoami)/Documents/vuln-reports/reports-$line.txt:/wpscan/output.txt -it --rm wpscanteam/wpscan --follow-redirection -t 2 -u "http://$line" --log /wpscan/output.txt
 done
